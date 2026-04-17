bf1919dc6348be66e0cfbb18176ea0133f0547b0
DarkEclipse31 <natchajong31@gmail.com>
2026-04-17 22:53:48 +0300

    UI, main + Res

--- app/src/main/java/com/example/space_colony_game/MainActivity.java
+++ app/src/main/java/com/example/space_colony_game/MainActivity.java
@@ -1,24 +1,64 @@
-package com.example.space_colony_game;
+package com.example.spaceapplication;
 
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+import com.example.spaceapplication.UI.StartFragment;
+import com.example.spaceapplication.data.SaveLoadManager;
+import com.example.spaceapplication.data.Storage;
+import com.example.spaceapplication.model.Pilot;
+import com.example.spaceapplication.model.Location;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
+    protected void onPause() {
+        super.onPause();
+        SaveLoadManager.saveGame(this);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        SaveLoadManager.saveGame(this);
+    }
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        
+        // Initialize game state
+        if (!SaveLoadManager.loadGame(this)) {
+            seedStarterCrew();
+            SaveLoadManager.saveGame(this);
+        }
+
+        if (savedInstanceState == null) {
+            openFragment(new StartFragment(), false);
+        }
+    }
+
+    public void openFragment(Fragment fragment, boolean addToBackStack) {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.fragment_container, fragment);
+        if (addToBackStack) {
+            transaction.addToBackStack(null);
+        }
+        transaction.commit();
+    }
+
+    //Set the Starting player - Pilot, Coins
+
+    private void seedStarterCrew() {
+        Storage.getInstance().resetGameState();
+        Storage.getInstance().getGameState().setCoins(300);
+
+        int id = Storage.getInstance().nextId();
+        Pilot nova = new Pilot(id, "Nova");
+        nova.setLocation(Location.QUARTERS);
+
+        Storage.getInstance().getGameState().getCrewMembers().add(nova);
     }
 }
