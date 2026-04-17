b424b0e2d93dbca13b592fa85d4bfbe4ab029946
Kaira <101194371+Kairaquits@users.noreply.github.com>
2026-04-17 23:05:55 +0300

    util

---
+++ app/src/main/java/com/example/space_colony_game/util/CrewImageResolver.java
@@ -0,0 +1,67 @@
+package com.example.space_colony_game.util;
+
+import com.example.spaceapplication.R;
+import com.example.spaceapplication.model.CrewRole;
+
+import java.util.Random;
+
+public class CrewImageResolver {
+    private static final Random RANDOM = new Random();
+
+    public static int getRandomImageRes(CrewRole role) {
+        int[] images;
+
+        switch (role) {
+            case ENGINEER:
+                images = new int[]{
+                        R.drawable.engineer_1,
+                        R.drawable.engineer_2,
+                        R.drawable.engineer_3,
+                        R.drawable.engineer_4,
+                        R.drawable.engineer_5
+                };
+                break;
+
+            case MEDIC:
+                images = new int[]{
+                        R.drawable.medic_1,
+                        R.drawable.medic_2,
+                        R.drawable.medic_3
+                };
+                break;
+
+            case PILOT:
+                images = new int[]{
+                        R.drawable.pilot_1,
+                        R.drawable.pilot_2,
+                        R.drawable.pilot_3,
+                        R.drawable.pilot_4,
+                        R.drawable.pilot_5
+                };
+                break;
+
+            case SCIENTIST:
+                images = new int[]{
+                        R.drawable.scientist_1,
+                        R.drawable.scientist_2,
+                        R.drawable.scientist_3
+                };
+                break;
+
+            case SOLDIER:
+            default:
+                images = new int[]{
+                        R.drawable.soldier_1,
+                        R.drawable.soldier_2,
+                        R.drawable.soldier_3,
+                        R.drawable.soldier_4,
+                        R.drawable.soldier_5,
+                        R.drawable.soldier_6,
+                        R.drawable.soldier_7
+                };
+                break;
+        }
+
+        return images[RANDOM.nextInt(images.length)];
+    }
+}
